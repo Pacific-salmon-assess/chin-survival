a3e62f4902b178ca2b91b77a707054c998f44f31
Freshwater <Cameron.Freshwater@dfo-mpo.gc.ca>
2024-12-29 11:32:45 -0800

    distinguish between generative model and simpler estimation model

--- R/dags_log_reg.R
+++ R/dags_log_reg.R
@@ -51,7 +51,7 @@ adjustmentSets(dag_full, exposure = "I", outcome = "S")
 # year effects on survival, size, and lipid
 # size and lipid effects on survival 
 
-## Updated version
+## generative model
 online_dag <- dagitty('
 dag {
 "apparent survival" [outcome,pos="0.665,1.409"]
@@ -85,155 +85,37 @@ year -> exploitation
 }
 ')
 
-exposure <- c("lipid", "size", "injury", "date", "stock", "year")
-outcome <- "surv"
-
-adjustment_sets <- adjustmentSets(online_dag, exposure, outcome)
-print(adjustment_sets)
-
-
-set.seed(123)
-
-# Sample size
-n <- 100
-
-# Simulate ancestor variables
-G <- rnorm(n)  # Genetic predisposition
-SES <- rnorm(n)  # Socio-economic status
-HC <- rnorm(n)  # Health consciousness
-
-# Simulate exposure variable X (physical exercise)
-X <- 0.5 * G + 0.3 * SES + 0.4 * HC + rnorm(n)
-X <- rnorm(n)
-
-# Simulate outcome variable Y (blood pressure)
-Y <- -0.6 * X + 0.2 * G + 0.1 * SES + 0.2 * HC + rnorm(n)
-
-# Create a data frame
-data <- data.frame(G, SES, HC, X, Y)
-# Fit the model
-model <- lm(Y ~ X + G + SES + HC, data = data)
-
-# Summarize the model
-summary(model)
-
-################################################################################
-
-## experiment with multimodel inference 
-
-library(rethinking)
-library(brms)
-library(ggdist)
-
-
-## Example 1: Chapter 5 H2 homework 
-# (https://sr2-solutions.wjakethompson.com/causes-confounds-colliders)
-
-data("WaffleDivorce")
-
-dat <- WaffleDivorce %>%
-  select(A = MedianAgeMarriage,
-         D = Divorce,
-         M = Marriage) %>%
-  mutate(across(everything(), standardize))
-
-d_model <- bf(D ~ 1 + A)
-a_model <- bf(A ~ 1 + M)
-
-b5h2 <- brm(d_model + a_model + set_rescor(FALSE),
-            data = dat, family = gaussian,
-            prior = c(prior(normal(0, 0.2), class = Intercept, resp = D),
-                      prior(normal(0, 0.5), class = b, resp = D),
-                      prior(exponential(1), class = sigma, resp = D),
-                      
-                      prior(normal(0, 0.2), class = Intercept, resp = A),
-                      prior(normal(0, 0.5), class = b, resp = A),
-                      prior(exponential(1), class = sigma, resp = A)),
-            iter = 4000, warmup = 2000, chains = 4, cores = 4, seed = 1234)
-
-summary(b5h2)
-
-
-as_draws_df(b5h2) %>%
-  as_tibble() %>%
-  select(.draw, b_D_Intercept:sigma_A) %>% 
-  expand(nesting(.draw, b_D_Intercept, b_A_Intercept, b_D_A, b_A_M,
-                 sigma_D, sigma_A),
-         m = seq(from = -2, to = 2, length.out = 30)) %>%
-  mutate(a_sim = rnorm(n(), mean = b_A_Intercept + b_A_M * m, sd = sigma_A),
-         d_sim = rnorm(n(), mean = b_D_Intercept + b_D_A * a_sim, sd = sigma_D)) %>%
-  pivot_longer(ends_with("_sim"), names_to = "name", values_to = "value") %>%
-  group_by(m, name) %>%
-  mean_qi(value, .width = c(0.89)) %>%
-  ungroup() %>%
-  mutate(name = case_when(name == "a_sim" ~ "Counterfactual M &rarr; A",
-                          TRUE ~ "Counterfactual M &rarr; A &rarr; D")) %>%
-  ggplot(aes(x = m, y = value, ymin = .lower, ymax = .upper)) +
-  facet_wrap(~name, nrow = 1) +
-  geom_smooth(stat = "identity") +
-  labs(x = "Manipulated M", y = "Counterfactual")
-
-
-## Chimpanzees adaptive priors example
-
-data(chimpanzees, package = "rethinking")
-d <- chimpanzees 
-d2 <- d %>% 
-  mutate(actor     = factor(actor),
-         block     = factor(block),
-         treatment = factor(1 + prosoc_left + 2 * condition),
-         # this will come in handy, later
-         labels    = factor(treatment,
-                            levels = 1:4,
-                            labels = c("r/n", "l/n", "r/p", "l/p")))
-
-b14.3 <- brm(data = d2, 
-      family = binomial,
-      pulled_left | trials(1) ~ 0 + treatment + (0 + treatment | actor) #+
-        # (0 + treatment | block)
-      ,
-      prior = c(prior(normal(0, 1), class = b),
-                prior(exponential(1), class = sd, group = actor),
-                # prior(exponential(1), class = sd, group = block),
-                prior(lkj(2), class = cor, group = actor)#,
-                # prior(lkj(2), class = cor, group = block)
-                ),
-      iter = 2000, warmup = 1000, chains = 4, cores = 4,  
-      seed = 4387510)
-stancode(b14.3)
-
-d$block_id <- d$block
-d$treatment <- 1L + d$prosoc_left + 2L*d$condition
-dat <- list(
-  L = d$pulled_left,
-  tid = d$treatment,
-  actor = d$actor,
-  block_id = as.integer(d$block_id)
+## recovery model
+online_dag2 <- dagitty('
+dag {
+"apparent survival" [outcome,pos="0.665,1.409"]
+"detection probability" [pos="-0.770,0.090"]
+"fork length" [exposure,pos="0.106,0.772"]
+"tagging date" [exposure,pos="-0.713,1.122"]
+condition [latent,pos="-0.103,0.591"]
+exploitation [exposure,pos="0.307,0.102"]
+injury [exposure,pos="-0.012,1.599"]
+lipid [exposure,pos="0.378,0.688"]
+stock [adjusted,pos="-0.869,0.298"]
+year [adjusted,pos="-0.397,-0.024"]
+"detection probability" -> "apparent survival"
+"fork length" -> "apparent survival"
+"tagging date" -> "apparent survival"
+"tagging date" -> condition
+condition -> "fork length"
+condition -> lipid
+exploitation -> "apparent survival"
+injury -> "apparent survival"
+lipid -> "apparent survival"
+stock -> "apparent survival"
+stock -> "detection probability"
+stock -> "tagging date"
+stock -> condition
+stock -> exploitation
+year -> "apparent survival"
+year -> "detection probability"
+year -> condition
+year -> exploitation
+}
+'
 )
-
-m14.3 <- ulam(
-  alist(
-    L ~ binomial(1,p),
-    logit(p) <- g[tid] + alpha[actor,tid] + beta[block_id,tid],
-    # adaptive priors - non-centered
-    transpars> matrix[actor,4]:alpha <-
-      compose_noncentered( sigma_actor , L_Rho_actor , z_actor ),
-    transpars> matrix[block_id,4]:beta <-
-      compose_noncentered( sigma_block , L_Rho_block , z_block ),
-    matrix[4,actor]:z_actor ~ normal( 0 , 1 ),
-    matrix[4,block_id]:z_block ~ normal( 0 , 1 ),
-    # fixed priors
-    g[tid] ~ normal(0,1),
-    vector[4]:sigma_actor ~ dexp(1),
-    cholesky_factor_corr[4]:L_Rho_actor ~ lkj_corr_cholesky( 2 ),
-    vector[4]:sigma_block ~ dexp(1),
-    cholesky_factor_corr[4]:L_Rho_block ~ lkj_corr_cholesky( 2 ),
-    # compute ordinary correlation matrixes from Cholesky factors
-    gq> matrix[4,4]:Rho_actor <<- Chol_to_Corr(L_Rho_actor),
-    gq> matrix[4,4]:Rho_block <<- Chol_to_Corr(L_Rho_block)
-  ) , data=dat , chains=4 , cores=4 , log_lik=TRUE )
-rethinking::stancode(m14.3)
-
-precis(m14.3,3,pars=c("alpha"))
-precis(m14.3,3,pars=c("Rho_actor"))
-
