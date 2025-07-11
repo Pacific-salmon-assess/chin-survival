3fd269d70c8656cabb2984b0ce63b0e1f2a6e59d
CamFreshwater <ccfreshwater@gmail.com>
2025-07-11 12:31:05 -0700

    update HTS sensitivity analysis

--- R/survival_logistic_reg_stan_sensitivity.R
+++ R/survival_logistic_reg_stan_sensitivity.R
@@ -4,14 +4,65 @@
 # Jan. 6, 2025
 
 library(tidyverse)
-library(rethinking)
 library(rstan)
+library(tidybayes)
 library(grid)
 
 rstan::rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
 det_dat_in <- readRDS(here::here("data", "surv_log_reg_data.rds")) %>% 
+  filter(
+    !is.na(focal_er_adj)
+  ) %>% 
+  droplevels()
+
+
+# import posterior estimates of detection probability based on CJS models
+# add high detection probability for WCVI in 2021 and 2022 because a) all 
+# tagged fish Robertson Creek and b) 100% of in-river detections observed on
+# multiple arrays
+post_p_wcvi <- data.frame(
+  stock_group = "WCVI",
+  year = c("2021", "2022"),
+  mean_logit_p = 5.64,
+  sd_logit_p = 1.09
+)
+post_p <- readRDS(here::here("data", "posterior_p.rds")) %>% 
+  rbind(., post_p_wcvi) %>% 
+  mutate(det_group_id = paste(as.character(stock_group), year, sep = "_")) %>% 
+  select(-c(stock_group, year))
+key <- det_dat_in %>% 
+  mutate(
+    det_group_id = ifelse(
+      grepl("Fraser", stock_group), "Fraser", as.character(stock_group)
+    ) %>% 
+      paste(., year, sep = "_")
+  ) %>% 
+  select(stock_group, year, det_group_id) %>% 
+  distinct() %>% 
+  arrange(stock_group, year, det_group_id) %>% 
+  left_join(., post_p, by = "det_group_id") %>% 
+  #replace NAs with placeholders (not used in fitting, replaced by priors)
+  mutate(
+    post = ifelse(is.na(mean_logit_p), 0, 1),
+    mean_logit_p = ifelse(is.na(mean_logit_p), 0.001, mean_logit_p),
+    sd_logit_p = ifelse(is.na(sd_logit_p), 1, sd_logit_p)
+  ) %>% 
+  # sort so that unobserved stocks follow observed
+  arrange(desc(post), stock_group, year, det_group_id) %>% 
+  mutate(
+    det_group_id_n = seq(1, nrow(.), by = 1)
+  )
+
+
+
+det_dat_in2 <- det_dat_in %>% 
+  left_join(
+    .,
+    key %>% select(stock_group, year, det_group_id_n), 
+    by = c("stock_group", "year")
+  ) %>% 
   #scale cov
   mutate(
     lipid_z = scale(lipid) %>% as.numeric(),
@@ -19,22 +70,33 @@ det_dat_in <- readRDS(here::here("data", "surv_log_reg_data.rds")) %>%
     wt_z = scale(wt) %>% as.numeric(),
     log_wt_z = scale(log(wt)) %>% as.numeric(),
     day_z = scale(year_day) %>% as.numeric(),
-    cyer_z = scale(focal_er) %>% as.numeric(),
-    terminal_p = as.factor(terminal_p),
+    cyer_z = scale(focal_er_adj) %>% as.numeric(),
     inj = as.integer(as.factor(adj_inj))
-    )
+  ) %>% 
+  mutate(
+    year = as.factor(year),
+    stock_group = factor(
+      stock_group, 
+      levels = c(
+        "Low Col.", "Up Col.", "WA_OR", "WCVI", "ECVI", 
+        "Fraser Spr. 1.x", "Fraser Sum. 1.2", "Fraser Sum. 0.3", "Fraser Fall", 
+        "North Puget", "South Puget"
+      )) %>% 
+      droplevels(),
+    yr = as.integer(year),
+    stk = as.integer(stock_group)
+  ) 
 
 
 # switch maturity stage
-det_dat1 <- det_dat_in %>% 
+det_dat1 <- det_dat_in2 %>% 
   filter(
-    !is.na(focal_er),
     stage_2 == "mature"
-  )
+  ) 
 
 
 # five days post-release
-mature_tags1 <- det_dat_in %>% 
+mature_tags1 <- det_dat_in2 %>% 
   filter(stage_1 == "mature") %>% 
   pull(vemco_code)
 five_d_tags <- readRDS(
@@ -51,9 +113,8 @@ five_d_tags <- readRDS(
   ) %>% 
   pull(vemco_code) %>% 
   unique()
-det_dat2 <- det_dat_in %>% 
+det_dat2 <- det_dat_in2 %>% 
   filter(
-    !is.na(focal_er),
     vemco_code %in% five_d_tags & vemco_code %in% mature_tags1
   ) 
 
@@ -61,185 +122,99 @@ det_dat2 <- det_dat_in %>%
 dat_list_in <- list(det_dat1, det_dat2)
 
 
-# import posterior predictions
-post_p_wcvi <- data.frame(
-  stock_group = "WCVI",
-  year = c("2021", "2022"),
-  mean_logit_p = 5.64,
-  sd_logit_p = 1.09
-)
-post_p <- readRDS(here::here("data", "posterior_p.rds")) %>% 
-  rbind(., post_p_wcvi) %>% 
-  mutate(det_group_id = paste(as.character(stock_group), year, sep = "_")) %>% 
-  select(-c(stock_group, year))
-
 
 ## FIT MODELS ------------------------------------------------------------------
 
-mod1 <- stan_model(here::here("R", "stan_models", "obs_surv_jll_cov2.stan"))
-post_list <- sens_list <- vector(mode = "list", length = 2)
+mod1 <- stan_model(
+  here::here("R", "stan_models", "obs_surv_jll_cov2_uninformative.stan")
+  )
+
+
+fit_list <- vector(mode = "list", length = 2)
 
 for (i in seq_along(dat_list_in)) {
   x <- dat_list_in[[i]]
   
-  # make key within for loop since datasets have different stock-year 
-  # combinations which impacts indexing
-  key <- x %>% 
-    mutate(
-      det_group_id = ifelse(
-        grepl("Fraser", stock_group), "Fraser", as.character(stock_group)
-      ) %>% 
-        paste(., year, sep = "_")
-    ) %>% 
-    select(stock_group, year, det_group_id) %>% 
-    distinct() %>% 
-    arrange(stock_group, year, det_group_id) %>% 
-    left_join(., post_p, by = "det_group_id") %>% 
-    #replace NAs with placeholders
-    mutate(
-      post = ifelse(is.na(mean_logit_p), 0, 1),
-      mean_logit_p = ifelse(is.na(mean_logit_p), 0.001, mean_logit_p),
-      sd_logit_p = ifelse(is.na(sd_logit_p), 1, sd_logit_p)
-    ) %>% 
-    # sort so that unobserved stocks follow observed
-    arrange(desc(post), stock_group, year, det_group_id) %>% 
-    mutate(
-      det_group_id_n = seq(1, nrow(.), by = 1)
-    )
-  
-  x2 <- x %>% 
-    left_join(
-      .,
-      key %>% select(stock_group, year, det_group_id_n), 
-      by = c("stock_group", "year")
-    ) %>% 
-    mutate(
-      lipid_z = scale(lipid) %>% as.numeric(),
-      fl_z = scale(fl) %>% as.numeric(),
-      wt_z = scale(wt) %>% as.numeric(),
-      log_wt_z = scale(log(wt)) %>% as.numeric(),
-      day_z = scale(year_day) %>% as.numeric(),
-      cyer_z = scale(focal_er) %>% as.numeric(),
-      year = as.factor(year),
-      stock_group = factor(
-        stock_group, 
-        levels = c(
-          "Low Col.", "Up Col.", "WA_OR", "WCVI", "ECVI", 
-          "Fraser Spr. Yr.", "Fraser Sum. Yr.", "Fraser Sum. 4.1", "Fraser Fall", 
-          "North Puget", "South Puget"
-        )) %>% 
-        droplevels(),
-      inj = as.integer(as.factor(adj_inj)),
-      term_p = as.integer(terminal_p),
-      yr = as.integer(year),
-      stk = as.integer(stock_group)
-    ) 
-  
   dat_list <- list(
-    N = nrow(x2),
-    N_year = length(unique(x2$year)),
-    N_stock = length(unique(x2$stk)),
+    N = nrow(x),
+    N_year = length(unique(x$year)),
+    N_stock = length(unique(x$stk)),
     N_det_id = length(unique(key$det_group_id_n)),
     N_det_id_obs = sum(key$post),
     
-    s_obs = as.integer(x2$term_det),
+    s_obs = as.integer(x$term_det),
     
     logit_det_sd = key$sd_logit_p,
     logit_det_mu = key$mean_logit_p,
     use_posterior = key$post,
-    det_group_id = x2$det_group_id_n,
+    det_group_id = x$det_group_id_n,
     
-    yr = x2$yr,
-    stk_n = x2$stk,
-    inj = x2$inj,
-    fl_z = x2$fl_z,
-    lipid_z = x2$lipid_z,
-    day_z = x2$day_z,
-    cyer_z = x2$cyer_z,
-    alpha_i = rep(2, length(unique(x2$inj)) - 1)
+    yr = x$yr,
+    stk_n = x$stk,
+    inj = x$inj,
+    fl_z = x$fl_z,
+    lipid_z = x$lipid_z,
+    day_z = x$day_z,
+    cyer_z = x$cyer_z,
+    alpha_i = rep(2, length(unique(x$inj)) - 1)
   )
   
-  sens_list[[i]] <- sampling(
+  
+  fit_list[[i]] <- sampling(
     mod1, data = dat_list,
     chains = 4, iter = 2000, warmup = 1000,
-    control = list(adapt_delta = 0.96)
+    control = list(adapt_delta = 0.97)
   )
-  post_list[[i]] <- extract.samples(sens_list[[i]])
 }
 
+
 saveRDS(
-  sens_list, 
-  here::here("data", "model_outputs", "hier_binomial_sens_list.rds")
-)
-saveRDS(
-  post_list, 
+  fit_list, 
   here::here("data", "model_outputs", "hier_binomial_sens_list_samps.rds")
 )
 
 
 ## import fit from main text 
-post_list <- readRDS(
+fit_list <- readRDS(
   here::here("data", "model_outputs", "hier_binomial_sens_list_samps.rds")
 )
-original_post <- readRDS(
-  here::here("data", "model_outputs", "hier_binomial_cyer_stan.rds")
-  ) %>% 
-  extract.samples()
+original_fit <- readRDS(
+  here::here(
+    "data", "model_outputs", "hier_binomial_cyer_stan_uninformative_adj.rds"
+    )
+  )
 
-post_list_full <- list(original_post, post_list[[1]], post_list[[2]])
+post_list_full <- list(original_fit, fit_list[[1]], fit_list[[2]])
 
 
 ## EXAMINE POSTERIOR PARAMETER ESTIMATES ---------------------------------------
 
 dat_names <- c("standard", "maturity", "tag effect")
-par_names <- c("alpha_s", "beta_ds", "beta_fs", "beta_ls", "beta_cs",
-               "beta_ds_cs", "beta_is")
 par_list <- vector(mode = "list", length = length(post_list_full))
 
-for(i in seq_along(par_list)) {
-  par_list[[i]] <- purrr::map(
-    par_names,
-    function (x) {
-      dum <- post_list_full[[i]][[x]]
-      dims <- dim(dum)
-      ndims <- if (is.null(dims)) 1 else length(dims)
-      
-      if (ndims == 1) {
-        # Scalar or vector
-        out <- data.frame(
-          parameter = x,
-          med = median(dum),
-          lo = rethinking::HPDI(dum, prob = 0.9)[1],
-          up = rethinking::HPDI(dum, prob = 0.9)[2]
-        )
-        
-      } else if (ndims == 2) {
-        # Matrix: iterations × parameter columns
-        colnames(dum) <- paste(x, seq_len(ncol(dum)), sep = "_")
-        out <- as.data.frame(dum) %>%
-          pivot_longer(
-            cols = everything(), names_to = "parameter", values_to = "est"
-          ) %>%
-          group_by(parameter) %>%
-          summarize(
-            med = median(est),
-            lo = rethinking::HPDI(est, prob = 0.9)[1],
-            up = rethinking::HPDI(est, prob = 0.9)[2],
-            .groups = "drop"
-          )
-        
-      } else {
-        warning(paste("Unsupported shape for parameter:", x))
-        out <- data.frame(parameter = x, med = NA, lo = NA, up = NA)
-      }
-    }
-  ) %>% 
-    bind_rows() %>% 
-    mutate(
-      dataset = dat_names[[i]]
-    )
-}
-
+par_list <- purrr::map2(
+  post_list_full, dat_names,
+  function (x, y) {
+    spread_draws(
+      x, 
+      beta_cs, beta_ls, beta_fs, beta_ds, beta_ds_cs, beta_is, alpha_s
+    ) %>% 
+      select(-starts_with(".")) %>% 
+      pivot_longer(
+        cols = everything(), names_to = "parameter", values_to = "est"
+      ) %>% 
+      group_by(parameter) %>% 
+      summarize(
+        med = median(est),
+        lo = rethinking::HPDI(est, prob = 0.9)[1],
+        up = rethinking::HPDI(est, prob = 0.9)[2]
+      ) %>% 
+      ungroup() %>% 
+      mutate(
+        dataset = y
+      )
+  }
+)
 
 all_pars <- par_list %>% 
   bind_rows() %>% 
