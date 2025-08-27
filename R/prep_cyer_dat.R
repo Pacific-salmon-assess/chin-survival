57a17e0e3ec9c0fba64ad44ebf33c3a3b2ac66df
CamFreshwater <ccfreshwater@gmail.com>
2025-08-27 16:05:36 -0700

    remove unused harvest data

--- R/prep_cyer_dat.R
+++ R/prep_cyer_dat.R
@@ -12,6 +12,118 @@ stock_key <- read.csv(
 
 ## CLEAN CYER DATA -------------------------------------------------------------
 
+
+## following imports mortality data from published CTC estimates stored in xls 
+# files (relatively slow so skip to saved RDS file)
+# stocks <- stock_key %>% 
+#   filter(!is.na(ctc_indicator)) %>% 
+#   pull(ctc_indicator) %>% 
+#   unique()
+# 
+# ## import marked data
+# sheet_names <- excel_sheets(
+#   here::here(
+#     "data", "harvest", "ctc_data", 
+#     "TCCHINOOK-25-XX-Appendix-C-Mortality-Distribution-Tables-Detailed-marked_noTBRSEAK.xlsx"
+#   ))
+# 
+# matching_sheets <- sheet_names[
+#   sapply(sheet_names, function(x) any(grepl(paste(stocks, collapse = "|"), x)))
+# ]
+# matching_sheets2 <- matching_sheets[
+#   sapply(matching_sheets, function(x) any(grepl("total mort", x)))
+# ]
+# sheet_ids <- which(sheet_names %in% matching_sheets2)
+# 
+# # identify sheets w/ relevant data and associated stock name
+# new_col_names <- c(
+#   "year", "cwt_n", "ages", "aabm_seak_t", "aabm_seak_n", "aabm_seak_s", 
+#   "aabm_nbc_t", "aabm_nbc_s", "aabm_wcvi_t", "aabm_wcvi_s", "isbm_nbc_t", 
+#   "isbm_nbc_n", "isbm_nbc_s", "isbm_sbc_t", "isbm_sbc_n", "isbm_sbc_s", 
+#   "isbm_n_falcon_t", "isbm_n_falcon_s", "isbm_s_falcon_t", "isbm_s_falcon_s", 
+#   "isbm_wac_n", "isbm_puget_n", "isbm_puget_s", "term_seak_t", "term_seak_n",
+#   "term_seak_s", "term_can_n", "term_can_s", "term_sus_t", "term_sus_n", 
+#   "term_sus_s", "stray", "esc", "comment"
+# ) 
+# 
+# cwt_dat_marked <- purrr::map2(
+#   sheet_ids, matching_sheets2, 
+#   function(x, y) {
+#     dum <- read_xlsx(
+#       here::here(
+#         "data", "harvest", "ctc_data", 
+#         "TCCHINOOK-25-XX-Appendix-C-Mortality-Distribution-Tables-Detailed-marked_noTBRSEAK.xlsx"
+#       ),
+#       sheet = x,
+#       skip = 6,
+#       col_names = FALSE
+#     )
+#     colnames(dum) <- new_col_names
+#     dum %>% 
+#       mutate(
+#         indicator = str_split(y, " ") %>% unlist() %>% .[1],
+#         mark = "marked"
+#       ) %>% 
+#       # remove five year averages at bottom of table
+#       filter(!grepl("-", year),
+#              ! year == "2024") 
+#   }
+# ) %>% 
+#   bind_rows()
+# 
+# cwt_dat_unmarked <- purrr::map2(
+#   sheet_ids, matching_sheets2, 
+#   function(x, y) {
+#     dum <- read_xlsx(
+#       here::here(
+#         "data", "harvest", "ctc_data", 
+#         "TCCHINOOK-25-XX-Appendix-C-Mortality-Distribution-Tables-Detailed-unmarked_noTBRSEAK.xlsx"
+#       ),
+#       sheet = x,
+#       skip = 6,
+#       col_names = FALSE
+#     )
+#     colnames(dum) <- new_col_names
+#     dum %>% 
+#       mutate(
+#         indicator = str_split(y, " ") %>% unlist() %>% .[1],
+#         mark = "unmarked"
+#       ) %>% 
+#       # remove five year averages at bottom of table
+#       filter(!grepl("-", year),
+#              ! year == "2024") 
+#   }
+# ) %>% 
+#   bind_rows()
+# 
+# 
+# cwt_dat_long <- rbind(cwt_dat_unmarked, cwt_dat_marked) %>%
+#   filter(comment == "ok") %>%
+#   mutate(indicator = paste(indicator, mark, sep = "_")) %>%
+#   pivot_longer(cols = c(starts_with("aabm"), starts_with("isbm"),
+#                         starts_with("term"), stray, esc),
+#                names_to = "strata", values_to = "percent_run") %>%
+#   mutate(
+#     year = as.numeric(year)
+#   )
+# 
+# cwt_dat_long2 <- cwt_dat_long %>%
+#   group_by(
+#     indicator, year
+#   ) %>%
+#   mutate(
+#     total_percent = sum(percent_run)
+#   ) %>%
+#   ungroup() %>%
+#   mutate(
+#     scaled_percent = percent_run / total_percent
+#   )
+# saveRDS(cwt_dat_long2,
+#         here::here(
+#           "data", "harvest", "cyer_dat_long.rds"
+#         ))
+
+
 cwt_dat_long2 <- readRDS(
   here::here(
     "data", "harvest", "cyer_dat_long.rds"
@@ -108,53 +220,3 @@ chi_dat3 <- chi_foo(cwt_dat_out3)
 saveRDS(rbind(cwt_dat_out3, chi_dat3),
         here::here("data", "harvest", "cleaned_cyer_dat_adj.rds"))
 
-
-
-## CLEAN PUGET SOUND CATCH/EFFORT DATA -----------------------------------------
-
-# creel rec catch data from https://wdfw.wa.gov/fishing/reports/creel/puget-annual?sample_date=3&ramp=&catch_area=
-paths <- list.files(path = here::here("data", "harvest", "puget_sound_catch"),
-                    pattern = "\\.csv$", 
-                    full.names = TRUE)
-ps_dat <- lapply(paths, read.csv) %>% 
-  bind_rows() %>% 
-  janitor::clean_names() %>% 
-  mutate(
-    dttm = lubridate::mdy(sample_date),
-    year = lubridate::year(dttm),
-    month = lubridate::month(dttm),
-    region = case_when(
-      catch_area %in% c(
-        "Area 4, Eastern portion",
-        "Area 3, La Push"
-      ) ~ "washington_coastal",
-      catch_area %in% c(
-        "Area 8-2, Ports Susan and Gardner",
-        "Area 8-1, Deception Pass, Hope Island, and Skagit Bay",
-        "Area 7, San Juan Islands",                             
-        "Area 6, East Juan de Fuca Strait",
-        "Bellingham Bay",
-        "Area 5, Sekiu and Pillar Point",                       
-        "Area 6-2, Eastern portion of Area 6",
-        "Area 6-1, Western portion of Area 6",
-        "Dungeness Bay"
-      ) ~ "north_ps",
-      TRUE ~ "south_ps"
-    )
-  ) %>% 
-  filter(
-    month > 4 & month < 11,
-    !region == "washington_coastal"
-  ) 
-
-ps_dat %>% 
-  group_by(year) %>%
-  mutate(total_chinook = sum(chinook, na.rm = TRUE)) %>%
-  group_by(region, year) %>%
-  summarize(
-    region_chinook = sum(chinook, na.rm = TRUE),
-    total_chinook = first(total_chinook),  # same for entire year
-    ppn_chinook = region_chinook / total_chinook,
-    .groups = "drop"
-  )
-# approximately 50% of Puget Sound catch occurs south of arrays
